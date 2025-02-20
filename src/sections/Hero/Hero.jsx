61689a7a48ab55bc3c33c4ebbe0ad9f0ceb623d7
iamsrt23 <ravi.teja.work95@gmail.com>
2025-02-20 21:01:48 +0530

    light-dark-theme-added

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,41 +1,67 @@
-import styles from './HeroStyles.module.css'
-import heroImg from '../../assets/hero-img.png'
-import themeIcon from '../../assets/sun.svg'
-import twitterIcon from '../../assets/twitter-light.svg'
-import gitHubIcon from '../../assets/github-light.svg'
-import linkedInIcon from '../../assets/linkedin-light.svg'
-import CV from '../../assets/cv.pdf'
+import styles from './HeroStyles.module.css';
+import heroImg from '../../assets/hero-img.png';
+import sun from '../../assets/sun.svg';
+import moon from '../../assets/moon.svg';
+import twitterLight from '../../assets/twitter-light.svg';
+import twitterDark from '../../assets/twitter-dark.svg';
+import githubLight from '../../assets/github-light.svg';
+import githubDark from '../../assets/github-dark.svg';
+import linkedinLight from '../../assets/linkedin-light.svg';
+import linkedinDark from '../../assets/linkedin-dark.svg';
+import CV from '../../assets/cv.pdf';
+import { useTheme } from '../../common/ThemeContent';
 
-const Hero = () => {
+function Hero() {
+  const { theme, toggleTheme } = useTheme();
+
+  const themeIcon = theme === 'light' ? sun : moon;
+  const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
+  const githubIcon = theme === 'light' ? githubLight : githubDark;
+  const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
   return (
-    <section id='hero' className={styles.container}>
+    <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
-        <img className={styles.hero} src={heroImg} alt="Profile Image" />
-        <img  className={styles.colorMode} src={themeIcon} alt='Color Mode Icon' />
+        <img
+          src={heroImg}
+          className={styles.hero}
+          alt="Profile picture of Harris Johnsen"
+        />
+        <img
+          className={styles.colorMode}
+          src={themeIcon}
+          alt="Color mode icon"
+          onClick={toggleTheme}
+        />
       </div>
       <div className={styles.info}>
-        <h1>Ravi<br />Teja</h1>
+        <h1>
+          Ravi Teja
+          <br />
+          Sure
+        </h1>
         <h2>Frontend Developer</h2>
         <span>
-          <a href='https://twitter.com/' target="_blank">
-          <img src={twitterIcon}  alt="X icon"/>
+          <a href="https://twitter.com/" target="_blank">
+            <img src={twitterIcon} alt="Twitter icon" />
           </a>
-          <a href='https://github.com/' target="_blank">
-          <img src={gitHubIcon}  alt="github icon"/>
+          <a href="https://github.com/" target="_blank">
+            <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href='https://linkedin.com/' target="_blank">
-          <img src={linkedInIcon}  alt="linkedin icon"/>
+          <a href="https://linkedin.com/" target="_blank">
+            <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
-        <p>
-          With a passion for developing modren React web apps for commercial bussinesses.
+        <p className={styles.description}>
+          With a passion for developing modern React web apps for commercial
+          businesses.
         </p>
         <a href={CV} download>
-          <button className='hover'> Resume </button>
+          <button className="hover">Resume</button>
         </a>
       </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
